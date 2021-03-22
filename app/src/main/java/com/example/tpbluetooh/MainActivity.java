449bdf6c8b0481f63e7053007ef71061faa4bb5d
Gauthier <gauthier.mure@gmail.com>
2021-03-22 10:54:19 +0100

    V0.1 + turn on BT

--- app/src/main/java/com/example/tpbluetooh/MainActivity.java
+++ app/src/main/java/com/example/tpbluetooh/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.tpbluetooh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView readbuffer, status;
     Button btOn, btOff, showpaired, discoverDev, makeDev;
     ListView listDev;
+    BluetoothAdapter bluetoothadapter;
+
+    private static final int  REQUEST_ENABLE_BT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.BTon:
+                TurnBTon();
                 break;
             case R.id.BToff:
                 break;
@@ -48,6 +55,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.Dicoverdev:
                 break;
+        }
+    }
+
+    public void TurnBTon(){
+        bluetoothadapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothadapter == null){
+            Toast.makeText(this,"cet appareil ne dispose pas du bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Intent enableBTintent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTintent,REQUEST_ENABLE_BT);
         }
     }
 }
